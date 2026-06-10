2a4acdaeb4bad9ca0bc0b7edfa18b0d00705abe0
MariaJRo <mariajose.rodriguez.mendoza@gmail.com>
2026-06-10 23:39:09 +0100

    Agregada animación en Home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,6 +5,43 @@ export const MainSection = () => {
   if (!divContent) return
   divContent.replaceChildren()
 
+  // Agregada animación en el Home
+  const starsContainer = document.createElement('div')
+  starsContainer.className = 'stars-container'
+
+  const classes = [
+    'star1',
+    'star2',
+    'star3',
+    'star4',
+    'star5',
+    'star6',
+    'star7',
+    'star8',
+    'star9',
+    'star10',
+    'star11',
+    'star12',
+    'star13',
+    'star14',
+    'star15',
+    'star16',
+    'star17',
+    'star18',
+    'star19',
+    'star20'
+  ]
+
+  for (let i = 0; i < classes.length; i++) {
+    const star = document.createElement('div')
+    star.className = 'star ' + classes[i]
+    starsContainer.appendChild(star)
+  }
+
+  divContent.appendChild(starsContainer)
+
+  // Agregada animación en el Home
+
   const welcome = document.createElement('h1')
   welcome.textContent = 'Welcome to Games Hub'
 
