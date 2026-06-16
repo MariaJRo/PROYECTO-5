506c9b454c672c45e1db4f2c7789834d0af52bb9
MariaJRo <mariajose.rodriguez.mendoza@gmail.com>
2026-06-16 18:49:17 +0100

    HOME a la izquierda

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,25 +6,26 @@ import './header.css'
 
 export const Header = (divApp) => {
   const header = document.createElement('header')
+  const buttonHome = document.createElement('button')
   const buttonXXO = document.createElement('button')
   const buttonWam = document.createElement('button')
   const buttonMemory = document.createElement('button')
-  const buttonHome = document.createElement('button')
+
   buttonHome.className = 'buttonHome'
 
+  buttonHome.textContent = 'HOME'
   buttonXXO.textContent = 'TicTacToe'
   buttonWam.textContent = 'Whack-a-mole'
   buttonMemory.textContent = 'Memory Game'
-  buttonHome.textContent = 'HOME'
 
+  buttonHome.addEventListener('click', MainSection)
   buttonMemory.addEventListener('click', initMemory)
   buttonWam.addEventListener('click', initWam)
   buttonXXO.addEventListener('click', initXXO)
-  buttonHome.addEventListener('click', MainSection)
 
+  header.appendChild(buttonHome)
   header.appendChild(buttonXXO)
   header.appendChild(buttonWam)
   header.appendChild(buttonMemory)
-  header.appendChild(buttonHome)
   divApp.appendChild(header)
 }
